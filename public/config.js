c209515eab52957040ee2343937dbff9946af293
ketan1829 <ketan.salvi21@gmail.com>
2023-12-10 10:08:53 +0000

    development branch

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-window.IP_SERVER = "http://localhost:21465/api/";
-window.IP_SOCKET_IO = "http://localhost:21465/";
+window.IP_SERVER = process.env.REACT_APP_API_BASE_URL || `http://localhost:${process.env.PORT || 21465}/api/`;
+window.IP_SOCKET_IO = process.env.REACT_APP_SOCKET_IO_BASE_URL || `http://localhost:${process.env.PORT || 21465}/`;
